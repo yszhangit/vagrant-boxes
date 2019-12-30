80491fb14b709719253538e03a5eec422496106b
yinshuzhang <yinshu.zhang.git@gmail.com>
2019-12-29 23:03:50 -0500

    verified with standalone setup

--- OracleDatabase/db_pri/createdb_testdb.sql
+++ OracleDatabase/db_pri/createdb_testdb.sql
@@ -1,17 +1,26 @@
+create spfile from pfile
+/
+
+alter system set enable_pluggable_database = false scope = spfile
+/
+
+shutdown abort
+
+startup nomount
+
 create database testdb
+USER SYS IDENTIFIED BY testdbpw
+USER SYSTEM IDENTIFIED BY testdbpw
 controlfile reuse
 logfile
-group  1 ('/opt/oracle/oradata/testdb/redo/log01a.rdo', '/opt/oracle/oradata/testdb/redo/log01b.rdo') size 50M,
-group  2 ('/opt/oracle/oradata/testdb/redo/log02a.rdo', '/opt/oracle/oradata/testdb/redo/log02b.rdo') size 50M,
-group  3 ('/opt/oracle/oradata/testdb/redo/log03a.rdo', '/opt/oracle/oradata/testdb/redo/log03b.rdo') size 50M
-datafile
-	'/opt/oracle/oradata/testdb/system01.dbf' size 250M autoextend on maxsize 500M
-sysaux datafile
-	'/opt/oracle/oradata/testdb/sysaux01.dbf' size 100M autoextend on maxsize 500M
-undo tablespace undotbs datafile
-	'/opt/oracle/oradata/testdb/undo01.dbf' size 50M
+group  1 ('/opt/oracle/oradata/testdb/redo/log01a.rdo', '/opt/oracle/oradata/testdb/redo/log01b.rdo') size 50M reuse,
+group  2 ('/opt/oracle/oradata/testdb/redo/log02a.rdo', '/opt/oracle/oradata/testdb/redo/log02b.rdo') size 50M reuse,
+group  3 ('/opt/oracle/oradata/testdb/redo/log03a.rdo', '/opt/oracle/oradata/testdb/redo/log03b.rdo') size 50M reuse
+datafile '/opt/oracle/oradata/testdb/system01.dbf' size 250M reuse autoextend on maxsize 500M
+sysaux datafile '/opt/oracle/oradata/testdb/sysaux01.dbf' size 100M reuse autoextend on maxsize 500M
+undo tablespace undotbs datafile '/opt/oracle/oradata/testdb/undo01.dbf' size 100M reuse
 DEFAULT TEMPORARY TABLESPACE temp 
-TEMPFILE '/opt/oracle/oradata/testdb/temp.dbf' size 75M
+TEMPFILE '/opt/oracle/oradata/testdb/temp.dbf' size 75M reuse
 CHARACTER SET AL32UTF8
 NATIONAL CHARACTER SET AL16UTF16
 EXTENT MANAGEMENT LOCAL
